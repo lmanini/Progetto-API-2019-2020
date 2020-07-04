4ef4d66b751d5af401395a38c10195a89ab99fad
lmanini <lmanini@tradingbots.it>
2020-07-04 17:51:57 +0200

    Initial commit : empty main.c

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Basic functioning of edU :
+ *
+ * 1. Read input one line at a time
+ * 2. For each line, parse it, store the index(es) given and recognize what command is given
+ * 3. Execute command
+ * 4. Go back to 1.
+ */
+
+/*
+ *************  TO DO LIST:  *************
+ *  1. Read input one line at a time
+ *  2. Parse read line
+ *  3. Inverse command function
+ *  4. Command functions
+ *      4.1 C command
+ *      4.2 D command
+ *      4.3 P command
+ *      4.4 U command
+ *      4.5 R command
+ *      4.6 Q command
+ */
+
 
 int main() {
-    printf("Hello, World!\n");
+
+
     return 0;
 }
